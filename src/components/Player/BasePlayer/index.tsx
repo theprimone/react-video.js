ddadc171bb6ec11d2cc1e09d2d81083eda59f239
Yuns Xie <yuns.xie@qq.com>
2019-12-27 15:00:40 +0800

    refactor: BasePlayer

---
+++ src/components/Player/BasePlayer/index.tsx
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import videojs, { VideoJsPlayer, VideoJsPlayerOptions } from 'video.js';
+import 'video.js/dist/video-js.css';
+import 'videojs-flash';
+import defaultSettings from '../../../../config/defaultSettings';
+import styles from './index.less';
+
+const { publicPath } = defaultSettings;
+
+const defaultOptions: VideoJsPlayerOptions = {
+  // aspectRatio: '16:9',
+  // fluid: true,
+  techOrder: ['html5', 'flash'],
+  controls: true,
+};
+
+const flashOptions = {
+  flash: {
+    swf: `${publicPath}video-js.swf`,
+  },
+};
+
+export interface BasePlayerProps {
+  options?: VideoJsPlayerOptions;
+  onReady?: (player: VideoJsPlayer) => void;
+  style?: React.CSSProperties;
+  className?: string;
+  videoClassName?: string;
+}
+
+export default class BasePlayer extends Component<BasePlayerProps> {
+  videoNode?: HTMLVideoElement;
+
+  player?: VideoJsPlayer;
+
+  componentDidMount() {
+    const { options, onReady = () => {} } = this.props;
+
+    const videoJsOptions: VideoJsPlayerOptions = {
+      ...defaultOptions,
+      ...flashOptions,
+      ...options,
+    };
+
+    // instantiate Video.js
+    this.player = videojs(this.videoNode, videoJsOptions, function onPlayerReady(
+      this: VideoJsPlayer,
+    ) {
+      // console.log('flash tech', videojs.getTech('flash'));
+      // console.log('html5 tech', videojs.getTech('html5'));
+      onReady(this);
+    });
+  }
+
+  // destroy player on unmount
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+
+  render() {
+    const { style, className, videoClassName } = this.props;
+    return (
+      <div style={style} className={classNames(styles.basePlayer, className)}>
+        {/* data-vjs-player ref: https://docs.videojs.com/tutorial-react.html */}
+        <div data-vjs-player="true">
+          <video
+            ref={node => {
+              if (node) {
+                this.videoNode = node;
+              }
+            }}
+            className={classNames('video-js', videoClassName)}
+          />
+        </div>
+      </div>
+    );
+  }
+}
